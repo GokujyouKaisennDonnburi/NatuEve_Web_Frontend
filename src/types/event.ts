755ea930d9e8f6525b16a8ca9dd39351af24e65d
kozo10110 <kd1363861@st.kobedenshi.ac.jp>
2026-07-09 10:38:04 +0900

    feat: tags?: string[];追加#89

--- src/types/event.ts
+++ src/types/event.ts
@@ -32,6 +32,8 @@ export type CreateEventRequest = {
   costs: EventCostInput[];
   // 持ち物リスト（任意）。
   items?: EventItemInput[];
+  // タグリスト（任意・各要素30文字以内・最大10件）。
+  tags?: string[];
   // 画像オブジェクトキーの一覧（任意）。
   imageObjectKeys?: string[];
   // PDF オブジェクトキーの一覧（任意・各要素255文字以内）。
