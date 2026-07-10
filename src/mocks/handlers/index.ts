7e7fc390083d0b2f5c72e3865288eacb97354a4e
kozo10110 <kd1363861@st.kobedenshi.ac.jp>
2026-07-10 11:51:35 +0900

    feat: タグ作成APIのMSWモック追加#97

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,5 +1,11 @@
 import { eventHandlers } from "./events";
+import { tagHandlers } from "./tags";
 import { uploadHandlers } from "./uploads";
 import { userHandlers } from "./user";
 
-export const handlers = [...userHandlers, ...eventHandlers, ...uploadHandlers];
+export const handlers = [
+  ...userHandlers,
+  ...eventHandlers,
+  ...tagHandlers,
+  ...uploadHandlers,
+];
