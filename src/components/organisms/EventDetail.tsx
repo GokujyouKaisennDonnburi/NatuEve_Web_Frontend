c0854d970868be03924b266897db9893f9f70e51
N_A <kd1338248@st.kobedenshi.ac.jp>
2026-08-04 10:10:35 +0900

    fix: 一覧画面にもどるリンクのテキスト変更#131

--- src/components/organisms/EventDetail.tsx
+++ src/components/organisms/EventDetail.tsx
@@ -77,7 +77,7 @@ export function EventDetail({
           className="h-auto w-fit cursor-pointer gap-1 p-0 has-[>svg]:px-0 text-sm font-normal text-slate-500 hover:text-sky-600 hover:no-underline"
         >
           <ChevronLeft className="h-4 w-4" />
-          一覧画面にもどる
+          イベント一覧にもどる
         </Button>
 
         {/* 主催者向けのボタン群（全体連絡ボタン、レポート作成ボタン） */}
